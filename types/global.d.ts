eb6d8f510f99d9275f060e33917304267d391ad6
prayface <prayface@163.com>
2023-01-29 16:22:58 +0800

    补充类型声明

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,6 +3,8 @@
 declare module "@vue/runtime-core" {
     export interface GlobalComponents {
         UiIcon: typeof import("various-ui")["UiIcon"];
+        VLoading: typeof import("various-ui")["VLoading"];
+        $loading: typeof import("various-ui")["$loading"];
     }
 
     interface ComponentCustomProperties {}
