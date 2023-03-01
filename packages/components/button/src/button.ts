a2d3844a0e6618a50bad915cfac2ea3e06ec7f02
prayface <prayface@163.com>
2023-03-01 18:12:27 +0800

    按钮增加链接模式,  回调遮罩颜色

--- packages/components/button/src/button.ts
+++ packages/components/button/src/button.ts
@@ -6,10 +6,12 @@ export const UiButtonType = {
     disabled: { type: Boolean, default: false },
     loading: { type: Boolean, default: false },
     simple: { type: Boolean, default: false },
+    target: { type: String as PropType<"_blank" | "_self">, default: "_self" },
     width: { type: Number },
     type: { type: String as PropType<UiTypes.type>, default: "info" },
     size: { type: String as PropType<UiTypes.size>, default: "default" },
     icon: { type: String },
+    href: { type: String },
 } as const;
 
 export type UiButtonProps = ExtractPropTypes<typeof UiButtonType>;
--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -1,5 +1,14 @@
 <template>
-    <div class="ui-button" v-loading="disabled" :class="className">
+    <a class="ui-button" v-loading="disabled" :target="target" :class="className" :href="href" v-if="href">
+        <button :type="nativeType" :style="styles" :disabled="disabled" @click="!disabled && emit('click', $event)">
+            <!-- 按钮图标 -->
+            <UiIcon :name="icon" v-if="icon" />
+            <!-- 内容插槽 -->
+            <slot></slot>
+        </button>
+    </a>
+
+    <div class="ui-button" v-loading="disabled" :class="className" v-else>
         <button :type="nativeType" :style="styles" :disabled="disabled" @click="!disabled && emit('click', $event)">
             <!-- 按钮图标 -->
             <UiIcon :name="icon" v-if="icon" />
