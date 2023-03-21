97d3bf7c2d350caeefda5be03dcafbc7b2910f87
prayface <prayface@163.com>
2023-03-21 15:03:54 +0800

    轮播图组件改造...

---
+++ packages/components/carousel/src/modules/composable.multiple-view.ts
@@ -0,0 +1,82 @@
+import { UiCarouselProps } from "../carousel";
+
+export type UiCarouselConstructorRefs = {
+    main?: HTMLDivElement;
+    container?: HTMLDivElement;
+    childrens?: HTMLElement[];
+    skipTimer?: NodeJS.Timer;
+    autoTimer?: NodeJS.Timer;
+    controls: boolean;
+    active: number;
+};
+
+export default class {
+    refs: UiCarouselConstructorRefs;
+    delay: number;
+    delayUp: number;
+
+    methods: {
+        init: () => void;
+        cutCarousel: (number: number, data?: any) => void;
+    };
+
+    constructor(refs: UiCarouselConstructorRefs, define: UiCarouselProps) {
+        this.refs = refs;
+        this.delay = define.transitionDelay / 1000;
+        this.delayUp = define.transitionDelay * 1.1;
+        this.methods = this.#useMthods(define);
+    }
+
+    #useMthods(define: UiCarouselProps) {
+        //? 初始化函数
+        const init = () => {
+            //* 获取模块容器失败则取消后续操作
+            if (!this.refs.main || !this.refs.container) return;
+            //* 初始化临时变量
+            if (this.refs.main.clientWidth < this.refs.container.clientWidth) {
+                this.refs.controls = true;
+                this.refs.container.style.transition = `all ${this.delay}s ease-in-out`;
+            }
+        };
+
+        //? 切换轮播图
+        const cutCarousel = (number: number, data?: any) => {
+            //* 检测是否满足运行条件
+            if (!this.refs.main || !this.refs.container || this.refs.skipTimer) return;
+            //* 判断是否左贴边
+            if (number * this.refs.main.clientWidth <= 0) {
+                this.refs.active = 0;
+                this.refs.container.style.transform = `translate3d(0, 0, 0)`;
+                this.refs.skipTimer = setTimeout(() => {
+                    this.refs.skipTimer = undefined;
+                }, this.delayUp);
+
+                return;
+            }
+
+            //* 判断是否右贴边
+            if ((number + 1) * this.refs.main.clientWidth >= this.refs.container.clientWidth) {
+                this.refs.active = this.refs.container.clientWidth / this.refs.main.clientWidth - 1;
+                this.refs.container.style.transform = `translate3d(${this.refs.active * -this.refs.main.clientWidth}px, 0, 0)`;
+                this.refs.skipTimer = setTimeout(() => {
+                    this.refs.skipTimer = undefined;
+                }, this.delayUp);
+                return;
+            }
+
+            //* 正常运行
+            this.refs.active = number;
+            this.refs.container.style.transform = `translate3d(${this.refs.active * -this.refs.main.clientWidth}px, 0, 0)`;
+            this.refs.skipTimer = setTimeout(() => {
+                this.refs.skipTimer = undefined;
+            }, this.delayUp);
+        };
+
+        return {
+            init,
+            cutCarousel,
+            triggerNext: () => cutCarousel(this.refs.active + 1),
+            triggerBack: () => cutCarousel(this.refs.active - 1),
+        };
+    }
+}
--- packages/components/carousel/src/modules/default.vue
+++ packages/components/carousel/src/modules/default.vue
@@ -6,164 +6,66 @@
         </div>
 
         <!-- 轮播图左侧箭头 -->
-        <div class="ui-carousel-control ui-carousel-left-control" @click="skip(active - 1)" v-if="arrow != 'never'">
+        <div class="ui-carousel-control ui-carousel-left-control" @click="cutCarousel(active - 1)" v-if="arrow != 'never'">
             <UiIcon name="arrow" />
         </div>
         <!-- 轮播图右侧箭头 -->
-        <div class="ui-carousel-control ui-carousel-right-control" @click="skip(active + 1)" v-if="arrow != 'never'">
+        <div class="ui-carousel-control ui-carousel-right-control" @click="cutCarousel(active + 1)" v-if="arrow != 'never'">
             <UiIcon name="arrow" />
         </div>
 
         <!-- 轮播图分页器 -->
         <div class="ui-carousel-schedules" v-if="pagination">
-            <template v-for="(value, index) in childrens" :key="index">
-                <div class="ui-carousel-schedule" :class="{ active: index == active }" @click="skip(index, value)"></div>
+            <template v-for="(value, index) in childrens">
+                <div class="ui-carousel-schedule" :class="{ active: index == active }" @click="cutCarousel(index, value)"></div>
             </template>
         </div>
     </div>
 </template>
 
-<script lang="ts" setup>
-import UiIcon from "@various/components/icon";
-import Composable from "./composable";
-import { ref, watch, onMounted, onBeforeUnmount } from "vue";
+<script lang="ts">
+import { defineComponent, reactive, toRefs, onMounted, onUnmounted } from "vue";
 import { UiCarouselPropsOption } from "../carousel";
+import ComposableDefault, { UiCarouselConstructorRefs } from "./composable.default";
+import Composable from "./composable";
+import UiIcon from "@various/components/icon";
 
-// DOM响应式变量声明
-const main = ref<HTMLDivElement>();
-const container = ref<HTMLDivElement>();
-const childrens = ref<HTMLElement[]>([]);
+export default defineComponent({
+    components: { UiIcon },
+    props: UiCarouselPropsOption,
+    setup(define, { expose }) {
+        //* 初始化响应式变量
+        const refs = reactive<UiCarouselConstructorRefs>({
+            main: undefined,
+            container: undefined,
+            childrens: undefined,
+            skipTimer: undefined,
+            autoTimer: undefined,
+            active: 0,
+        });
 
-// 响应式变量声明
-const active = ref<number>(0);
-const skipTimer = ref<NodeJS.Timer>();
-const autoTimer = ref<NodeJS.Timer>();
+        //* 实例化响应式变量
+        const composable = new Composable(define);
+        const composableDefault = new ComposableDefault(refs, define);
 
-// 获取Props配置
-const define = defineProps(UiCarouselPropsOption);
+        //* 挂载函数
+        onMounted(() => composableDefault.methods.init());
 
-// 静态变量计算
-const delay = define.transitionDelay / 1000;
-const delayUp = define.transitionDelay * 1.1;
+        //* 卸载函数
+        onUnmounted(() => {
+            refs.skipTimer && clearTimeout(refs.skipTimer);
+            refs.autoTimer && clearInterval(refs.autoTimer);
+            composableDefault.watchs.stopAutoPlay && composableDefault.watchs.stopAutoPlay();
+        });
 
-// 实例化组合类
-const composable = new Composable(define);
+        //* 导出函数
+        expose({ ...composableDefault.methods });
 
-// 计算数据获取
-const { style } = composable.computeds;
-
-// 初始化函数
-const init = () => {
-    //* 获取模块容器失败则取消后续操作
-    if (!main.value || !container.value || !container.value?.children?.length) return;
-    //* 初始化临时变量
-    const config = { size: 0 };
-    //* 初始化childrens
-    childrens.value = [];
-    for (let i = 0; i < container.value.children.length; i++) {
-        //* 获取node节点
-        const node = container.value.children[i] as HTMLElement;
-        //* 设置node的偏移
-        node.style.transform = `translate3d(${100 * i}%, 0, 0)`;
-        //* 偏移量计算
-        config.size += node.clientWidth;
-        //* 缓存node节点
-        childrens.value.push(node);
-        //* 调高第一张轮播的层级
-        if (i == 0) {
-            node.style.zIndex = "1";
-        }
-    }
-
-    //* 无限滚动的辅助内容生成
-    if (define.loop) {
-        const nodeHeader = childrens.value[childrens.value.length - 1].cloneNode(true) as HTMLElement;
-        const nodeFooter = childrens.value[0].cloneNode(true) as HTMLElement;
-        if (nodeHeader && nodeFooter) {
-            //* 样式调整
-            nodeHeader.style.transform = "translate3d(-100%, 0, 0)";
-            nodeFooter.style.transform = `translate3d(${container.value.children.length * 100}%, 0, 0)`;
-            //* 插入node
-            container.value.insertBefore(nodeHeader, childrens.value[0]);
-            container.value.appendChild(nodeFooter);
-        }
-    }
-};
-
-// 轮播切换函数
-const skip = (number: number, data?: any) => {
-    //* 检测是否满足运行条件
-    if (!container.value || skipTimer.value) return;
-    //* 无限滚动触边处理
-    if (define.loop && (number >= childrens.value.length || number < 0)) {
-        autoTimer.value && clearInterval(autoTimer.value);
-        container.value.style.transition = `all ${delay}s ease-in-out`;
-        container.value.style.transform = `translate3d(${number * -100}%, 0, 0)`;
-        if (number >= childrens.value.length) {
-            //* 分页器设置
-            active.value = 0;
-            skipTimer.value = setTimeout(() => {
-                autoTimer.value = setInterval(() => skip(active.value + 1), define.delay);
-                skipTimer.value = undefined;
-                //* 检测是否满足运行条件
-                if (!container.value) return;
-                container.value.style.transition = "none";
-                container.value.style.transform = `translate3d(0, 0, 0)`;
-            }, delayUp);
-        } else {
-            //* 分页器设置
-            active.value = childrens.value.length - 1;
-            skipTimer.value = setTimeout(() => {
-                autoTimer.value = setInterval(() => skip(active.value + 1), define.delay);
-                skipTimer.value = undefined;
-                //* 检测是否满足运行条件
-                if (!container.value) return;
-                container.value.style.transition = "none";
-                container.value.style.transform = `translate3d(-${active.value * 100}%, 0, 0)`;
-            }, delayUp);
-        }
-
-        return;
-    }
-
-    //* 触边处理
-    if (number >= childrens.value.length || number < 0) return;
-    else {
-        active.value = number;
-        autoTimer.value && clearInterval(autoTimer.value);
-        container.value.style.transition = `all ${delay}s ease-in-out`;
-        container.value.style.transform = `translate3d(-${number * 100}%, 0, 0)`;
-        skipTimer.value = setTimeout(() => {
-            autoTimer.value = setInterval(() => skip(active.value + 1), define.delay);
-            skipTimer.value = undefined;
-        }, delayUp);
-    }
-};
-
-// 前进函数
-const next = () => skip(active.value + 1);
-// 后退函数
-const back = () => skip(active.value - 1);
-
-// 侦听器回调函数
-const watchStop = watch(
-    () => define.autoplay,
-    () => {
-        autoTimer.value = setInterval(() => skip(active.value + 1), define.delay);
+        return {
+            ...toRefs(refs),
+            ...composable.computeds,
+            ...composableDefault.methods,
+        };
     },
-    { immediate: true }
-);
-
-// 挂载函数
-onMounted(() => init());
-
-// 卸载函数
-onBeforeUnmount(() => {
-    watchStop && watchStop();
-    skipTimer && clearTimeout(skipTimer.value);
-    autoTimer && clearInterval(autoTimer.value);
 });
-
-// 函数导出
-defineExpose({ skip, next, back });
 </script>
