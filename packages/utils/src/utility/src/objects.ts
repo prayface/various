03f9d2c20670ed7a2763eca49637516fa5bc5e18
prayface <prayface@163.com>
2023-07-31 14:32:10 +0800

    修复objectPick函数异常的BUG

--- packages/utils/src/utility/src/objects.ts
+++ packages/utils/src/utility/src/objects.ts
@@ -5,7 +5,7 @@ export const objectPick = (data: { [name: string]: any }, handler: (value: any) 
     else {
         const result: { [name: string]: any } = {};
         for (const i in data) {
-            if (result.hasOwnProperty(i) && (!reserve || !reserve.includes(i)) && handler(data[i])) {
+            if (data.hasOwnProperty(i) && (!reserve || !reserve.includes(i)) && handler(data[i])) {
                 result[i] = data[i];
             }
         }
