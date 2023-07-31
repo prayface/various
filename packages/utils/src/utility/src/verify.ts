4c89cceb117e82c2721e032b6b00b90da66d0d0e
prayface <prayface@163.com>
2023-07-31 16:14:32 +0800

    修改校验函数返回类型

--- packages/utils/src/utility/src/verify.ts
+++ packages/utils/src/utility/src/verify.ts
@@ -1,6 +1,6 @@
-export const isBoolean = (value: any) => Object.prototype.toString.call(value) == "[object Boolean]";
-export const isObject = (value: any) => Object.prototype.toString.call(value) == "[object Object]";
-export const isString = (value: any) => typeof value == "string";
-export const isNumber = (value: any) => typeof value == "number";
-export const isArray = (value: any) => Array.isArray(value);
-export const isDate = (value: any) => Object.prototype.toString.call(value) == "[object Date]";
+export const isBoolean = (value: any): value is boolean => Object.prototype.toString.call(value) == "[object Boolean]";
+export const isObject = (value: any): value is object => Object.prototype.toString.call(value) == "[object Object]";
+export const isString = (value: any): value is string => typeof value == "string";
+export const isNumber = (value: any): value is number => typeof value == "number";
+export const isArray = (value: any): value is any[] => Array.isArray(value);
+export const isDate = (value: any): value is Date => Object.prototype.toString.call(value) == "[object Date]";
