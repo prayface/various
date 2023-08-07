869231ed37fb4f384095da32c04e24f3277d1981
prayface <prayface@163.com>
2023-08-07 09:51:09 +0800

    分页器增加items插槽

--- packages/components/pagination/src/pagination.vue
+++ packages/components/pagination/src/pagination.vue
@@ -1,22 +1,27 @@
 <template>
     <div class="ui-pagination" v-if="total">
-        <div class="ui-pagination-items" v-if="items">{{ info }}</div>
+        <!-- * 分页信息 -->
+        <div class="ui-pagination-items" v-if="items">
+            <slot name="items">{{ info }}</slot>
+        </div>
+
+        <!-- * 分页控制器 -->
         <div class="ui-pagination-controls">
-            <!-- 后退控制器 -->
+            <!-- * 后退控制器 -->
             <div class="ui-pagination-control" @click="back" :class="{ 'ui-disabled-status': modelValue <= 1 }">
                 <UiIcon name="arrow" class="ui-pagination-next" />
             </div>
 
             <template v-for="control in controls" :key="control.type + control.value">
-                <!-- 跳跃控制器 -->
+                <!-- * 跳跃控制器 -->
                 <div v-if="control.type == 'skip'" class="ui-pagination-control" @click="cutNumber(modelValue + skip * control.value)">...</div>
-                <!-- 选择控制器 -->
+                <!-- * 选择控制器 -->
                 <div v-else class="ui-pagination-control" :class="{ 'ui-active': control.active }" @click="cutNumber(control.value)">
                     {{ control.value }}
                 </div>
             </template>
 
-            <!-- 前进控制器 -->
+            <!-- * 前进控制器 -->
             <div class="ui-pagination-control" @click="next" :class="{ 'ui-disabled-status': modelValue >= total }">
                 <UiIcon name="arrow" class="ui-pagination-back" />
             </div>
