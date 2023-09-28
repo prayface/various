c14320d5dd670832560e8f8bb870e5d5c762e49f
vicki <vicki_2018@163.com>
2023-09-28 18:04:24 +0800

    时间选择器，增加禁止选择时间

--- develop/src/views/Home.vue
+++ develop/src/views/Home.vue
@@ -6,6 +6,14 @@
                 <UiDatePicker v-model="data.date" mode="month" name="date" style="width: 240px; margin-top: 20px" />
             </UiFormItem>
         </UiForm>
+
+        <UiDatePicker
+            v-model="date2"
+            mode="month"
+            placeholder="开始时间"
+            :disabledDateStart="new Date(date3)"
+            style="width: 240px; margin-top: 20px" />
+        <UiDatePicker v-model="date3" mode="month" placeholder="截止时间" style="width: 240px; margin-top: 20px" @change="change()" />
     </div>
 </template>
 
@@ -13,6 +21,21 @@
 import { ref, reactive } from "vue";
 
 const date1 = ref("");
+const date2 = ref("");
+const date3 = ref("");
+const disabledDate1 = ref({
+    start: new Date(),
+    end: new Date("2025/08"),
+});
+
+const change = () => {
+    console.log("change");
+    if (date2.value > date3.value) {
+        date2.value = "";
+        console.log("date2", date2.value);
+    }
+};
+
 const data = reactive({
     date: "",
 });
@@ -22,6 +45,9 @@ const rules = {
         {
             trigger: "change",
             verify: (data) => {
+                const date = new Date();
+                const currentDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}`;
+                console.log(data.date, currentDate, data.date >= currentDate);
                 return { verify: false, message: "校验不通过, 奈我何!!!" };
             },
         },
