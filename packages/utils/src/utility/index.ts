f68d638277e603f6c7148c9103e787ae7d3b77e2
prayface <prayface@163.com>
2023-11-15 17:52:31 +0800

    函数库更新

--- packages/utils/src/utility/index.ts
+++ packages/utils/src/utility/index.ts
@@ -1,21 +1,20 @@
 //* 函数库
-import Maths from "mathjs";
-import Lodash from "lodash";
+import _ from "lodash";
 //* 自定义函数
 export * from "./src/random";
 export * from "./src/objects";
 //* 功能函数
-export const upperFirst = Lodash.upperFirst;
-export const cloneDeep = Lodash.cloneDeep;
+export const upperFirst = _.upperFirst;
+export const cloneDeep = _.cloneDeep;
 //* 校验函数
-export const isBoolean = Lodash.isBoolean;
-export const isObject = Lodash.isObject;
-export const isString = Lodash.isString;
-export const isNumber = Lodash.isNumber;
-export const isArray = Lodash.isArray;
-export const isDate = Lodash.isDate;
+export const isBoolean = _.isBoolean;
+export const isObject = _.isObject;
+export const isString = _.isString;
+export const isNumber = _.isNumber;
+export const isArray = _.isArray;
+export const isDate = _.isDate;
 //* 数学函数
-export const multiply = Maths.multiply;
-export const subtract = Maths.subtract;
-export const divide = Maths.divide;
-export const add = Maths.add;
+export const multiply = _.multiply;
+export const subtract = _.subtract;
+export const divide = _.divide;
+export const add = _.add;
